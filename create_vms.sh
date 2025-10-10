c776e588e685fa5a73ea625fc24a995943904654
Noname <>
2025-10-11 01:14:55 +0900

    fix: update network configurations for WAN and LAN VMs to correct interface assignments and enhance documentation

--- create_vms.sh
+++ create_vms.sh
@@ -260,51 +260,45 @@ COMMON_CORES=2
 COMMON_MEMORY=2048
 COMMON_DISK="16G"
 
-# VM 1000: wan0 (NAT Gateway for WAN-0)
-# net0 (eth0): WAN side - connected to vmbr00 (Global IP-0) with DHCP
-# net1 (eth1): LAN side - connected to vmbr10 (Translated Global IP-0) with static IP
+# VM 1000: wan0
 create_wan_vm 1000 "wan0" ${COMMON_CORES} ${COMMON_MEMORY} ${COMMON_DISK} ${SNIPPET_FILE}
 qm set 1000 --net0 virtio,bridge=vmbr00 --ipconfig0 ip=dhcp
 qm set 1000 --net1 virtio,bridge=vmbr10 --ipconfig1 ip=127.0.10.1/24
-qm set 1000 --net5 virtio,bridge=admin --ipconfig1 ip=172.0.0.100/24
+qm set 1000 --net2 virtio,bridge=admin --ipconfig2 ip=172.0.0.10/24
 qm set 1000 --nameserver "1.1.1.1 1.0.0.1"
 
-# VM 1001: wan1 (NAT Gateway for WAN-1)
-# net0 (eth0): WAN side - connected to vmbr01 (Global IP-1) with DHCP
-# net1 (eth1): LAN side - connected to vmbr11 (Translated Global IP-1) with static IP
+# VM 1001: wan1
 create_wan_vm 1001 "wan1" ${COMMON_CORES} ${COMMON_MEMORY} ${COMMON_DISK} ${SNIPPET_FILE}
 qm set 1001 --net0 virtio,bridge=vmbr01 --ipconfig0 ip=dhcp
 qm set 1001 --net1 virtio,bridge=vmbr11 --ipconfig1 ip=127.0.11.1/24
-qm set 1001 --net5 virtio,bridge=admin --ipconfig1 ip=172.0.0.101/24
+qm set 1001 --net2 virtio,bridge=admin --ipconfig2 ip=172.0.0.11/24
 qm set 1001 --nameserver "1.1.1.1 1.0.0.1"
 
-# VM 1002: router (NextRouter main VM - 8 CPU, 8GB RAM, 32GB Disk)
-# net0 (eth0): WAN-0 side - connected to vmbr10 (receives NAT'd traffic from wan0)
-# net1 (eth1): WAN-1 side - connected to vmbr11 (receives NAT'd traffic from wan1)
-# net2 (eth2): LAN side - connected to vmbr02 (Router output to LAN)
+# VM 1003: lan0
+create_vm 1003 "lan0" ${COMMON_CORES} ${COMMON_MEMORY} ${COMMON_DISK}
+qm set 1003 --net0 virtio,bridge=vmbr03 --ipconfig0 ip=dhcp
+qm set 1003 --net1 virtio,bridge=admin --ipconfig1 ip=172.0.0.13/24
+
+# VM 1004: lan1
+create_vm 1004 "lan1" ${COMMON_CORES} ${COMMON_MEMORY} ${COMMON_DISK}
+qm set 1004 --net0 virtio,bridge=vmbr03 --ipconfig0 ip=dhcp
+qm set 1004 --net1 virtio,bridge=admin --ipconfig1 ip=172.0.0.14/24
+
+# VM 1005: lan2 (Assuming name lan2 as lan1 is duplicated)
+create_vm 1005 "lan2" ${COMMON_CORES} ${COMMON_MEMORY} ${COMMON_DISK}
+qm set 1005 --net0 virtio,bridge=vmbr03 --ipconfig0 ip=dhcp
+qm set 1005 --net1 virtio,bridge=admin --ipconfig1 ip=172.0.0.15/24
+
+
+# --- VM Group 2 (8 CPU, 8GB RAM, 32GB Disk) ---
+
+# VM 1002
 create_vm 1002 "router" 8 8192 "32G"
 qm set 1002 --net0 virtio,bridge=vmbr10 --ipconfig0 ip=127.0.10.10/24,gw=127.0.10.1
 qm set 1002 --net1 virtio,bridge=vmbr11 --ipconfig1 ip=127.0.11.10/24,gw=127.0.11.1
-qm set 1002 --net2 virtio,bridge=vmbr02 --ipconfig2 ip=192.168.100.1/24
-qm set 1002 --net5 virtio,bridge=admin --ipconfig1 ip=172.0.0.102/24
+qm set 1002 --net2 virtio,bridge=vmbr12 --ipconfig2 ip=127.0.12.10/24
+qm set 1002 --net3 virtio,bridge=admin --ipconfig3 ip=172.0.0.12/24
 qm set 1002 --nameserver "1.1.1.1 1.0.0.1"
-
-# VM 1003: lan0 (LAN device 0)
-# net0 (eth0): Connected to vmbr03 (LAN input) with DHCP
-create_vm 1003 "lan0" ${COMMON_CORES} ${COMMON_MEMORY} ${COMMON_DISK}
-qm set 1003 --net0 virtio,bridge=vmbr03 --ipconfig0 ip=dhcp
-qm set 1003 --net5 virtio,bridge=admin --ipconfig1 ip=172.0.0.103/24
-
-# VM 1004: lan1 (LAN device 1)
-# net0 (eth0): Connected to vmbr03 (LAN input) with DHCP
-create_vm 1004 "lan1" ${COMMON_CORES} ${COMMON_MEMORY} ${COMMON_DISK}
-qm set 1004 --net0 virtio,bridge=vmbr03 --ipconfig0 ip=dhcp
-qm set 1004 --net5 virtio,bridge=admin --ipconfig1 ip=172.0.0.104/24
-# VM 1005: lan2 (LAN device 2)
-# net0 (eth0): Connected to vmbr03 (LAN input) with DHCP
-create_vm 1005 "lan2" ${COMMON_CORES} ${COMMON_MEMORY} ${COMMON_DISK}
-qm set 1005 --net0 virtio,bridge=vmbr03 --ipconfig0 ip=dhcp
-qm set 1005 --net5 virtio,bridge=admin --ipconfig1 ip=172.0.0.105/24
 
 
 echo "--- All VMs created ---"
@@ -317,6 +311,16 @@ echo "  - net0 (eth0): WAN interface with DHCP (connects to external network)"
 echo "  - net1 (eth1): LAN interface (connects to router VM)"
 echo "  - IP forwarding and NAT are enabled automatically via cloud-init"
 echo "  - VMs will reboot after initial setup to apply all settings"
+echo ""
+echo "=== Admin Bridge Configuration ==="
+echo "All VMs are connected to the 'admin' bridge (172.0.0.0/24) for SSH access from Proxmox:"
+echo "  - wan0:   172.0.0.10"
+echo "  - wan1:   172.0.0.11"
+echo "  - router: 172.0.0.12"
+echo "  - lan0:   172.0.0.13"
+echo "  - lan1:   172.0.0.14"
+echo "  - lan2:   172.0.0.15"
+echo "  - Proxmox: 172.0.0.1"
 echo ""
 
 read -p "Do you want to start all created VMs now? (y/N) " -n 1 -r
